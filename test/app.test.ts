570a59ae9084904024144c1feb2d851cbf75d549
Martin Karry <martin@karry.de>
2023-12-08 12:37:35 +0100

    added jest

---
+++ test/app.test.ts
@@ -0,0 +1,9 @@
+describe("Default tests", ()=>{
+    test("true === true", ()=>{
+        expect(true).toBe(true);
+    })
+
+    test("false === false", ()=>{
+        expect(false).toBe(false);
+    })
+})
