6d9095fa7c0c89cf5cc68aa91bc9b4dd7cc4a737
Martin Karry <martin@karry.de>
2023-12-08 12:19:22 +0100

    Changed to express service

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("Hello THM - MNI!")
+import express, { Express } from "express";
+
+const PORT = 3000;
+
+const server: Express = express()
+
+server.get("/", (req, res) => {
+    res.send("Hello THM!");
+})
+
+server.listen(PORT, () => {
+    console.log(`Server startet on PORT ${PORT}`)
+})
